333a9e9c6d48fb87da2de100a5574243cd4492ca
OIDEMA <k-goda@oidema-jp.com>
2021-12-29 14:39:49 +0900

    slotの使い方イメージ

---
+++ components/test.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <main>
+      <!-- ここにメインコンテンツ -->
+    </main>
+    <footer>
+      <!-- ここにフッターコンテンツ -->
+    </footer>
+  </v-container>
+</template>
